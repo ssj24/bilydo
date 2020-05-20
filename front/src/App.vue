34bc405b8db30edf4fecec7daeb12c38c66999a6
sujee <totquf824@gmail.com>
2020-05-20 22:17:55 +0900

    Update main page S02P31B207-29

--- front/src/App.vue
+++ front/src/App.vue
@@ -6,13 +6,19 @@
         app 
         color="#fff" 
         height="66"
+        class="navBar"
       >
-        <v-toolbar-title>빌리도</v-toolbar-title>
+        <v-toolbar-title style="font-family: 'ON-IGothic'; color: #df4c73; font-size: 2em; font-weight: bolder;">빌리도</v-toolbar-title>
 
         <v-spacer></v-spacer>
-
-        <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
+        <v-btn icon style="color: black; font-size: 1em; font-weight: 600; margin-right: 20px;">
+          둘러보기
+        </v-btn>
+        <v-btn icon v-if="true" style="color: black; font-size: 1em; font-weight: 600; margin-right: 10px;">
+          로그인
+        </v-btn>
+        <v-btn icon v-else style="color: black; font-size: 1em; font-weight: 600; margin-right: 10px;">
+          마이페이지
         </v-btn>
       </v-app-bar>
     </v-card>
@@ -37,5 +43,8 @@ export default Vue.extend({
 });
 </script>
 <style lang="scss">
-
+.navBar {
+  border-top: 2px solid #df4c73 !important;
+  box-shadow: 1px 1px 1px #ccc !important;
+}
 </style>
